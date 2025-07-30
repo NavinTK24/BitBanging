664d0e808df7b16027c8cd224d0d0d20f2e2ad3d
NavinTK24 <tk.navin.24@gmail.com>
2025-07-30 13:52:12 +0530

    updated code for 12c

--- wireH.cpp
+++ wireH.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 #define SDApin 9
 #define SCLpin 11
-#define ADDRESS1 0x40
+String ADDRESS1 = "10000001"; //0x40 for reading
 
 void setup() {
     Serial.begin(9600);
@@ -31,50 +31,68 @@ void i2cEnd() {
     delayMicroseconds(5);
 }
 
-uint8_t i2cWriteData(uint8_t data) {
+int twoPower(int n) {
+    int result = 1;
+    for(int i=0; i<n; i++) {
+        result*=2;
+    }
+
+    return result;
+}
+
+int i2cWriteData(char data) {
     pinMode(SCLpin, OUTPUT);
     pinMode(SDApin, OUTPUT);
 
     for(int i=0; i<8; i++) {
+        digitalWrite(SDApin, ((data & (1<<(7-i)))? HIGH : LOW));
+        delayMicroseconds(5);
         digitalWrite(SCLpin, HIGH);
         delayMicroseconds(5);
-        digitalWrite(SDApin, (data & (1<<(7-i)))? HIGH : LOW);
-        delayMicroseconds(5);
         digitalWrite(SCLpin, LOW);
+        delayMicroseconds(5);
+        digitalWrite(SDApin, LOW);
         delayMicroseconds(5);
     }
     pinMode(SDApin, INPUT);
+    digitalRead(SDApin);
+    delayMicroseconds(5);
     digitalWrite(SCLpin, HIGH);
     delayMicroseconds(5);
-    digitalRead(SDApin);
-
-    delayMicroseconds(5);
+    digitalWrite(SCLpin, LOW);
 }
 
-char i2cReadData() {
-    pinMode(SDApin, INPUT_PULLUP);
-    pinMode(SCLpin, OUTPUT);
+int i2cReadData() {
     char c = 0;
+    int dec = 0;
+    pinMode(SDApin, INPUT_PULLUP);
+    delayMicroseconds(5);
+    pinMode(SCLpin, OUTPUT);
     
     for(int i=0; i<8; i++) {
-        digitalWrite(SCLpin, HIGH);
-        delayMicroseconds(5);
+        digitalRead(SDApin);
         c<<=1;
         if(digitalRead(SDApin)) {
             c |= 1;
-        }
+            dec += twoPower(7-i);
+        } 
+        delayMicroseconds(5);
+        digitalWrite(SCLpin, HIGH);
+        delayMicroseconds(5);
 
         digitalWrite(SCLpin, LOW);
         delayMicroseconds(5);
     }
-
+    // pinMode(SDApin, INPUT_PULLUP);
+    // delayMicroseconds(2);
     pinMode(SDApin, OUTPUT);
+    digitalWrite(SDApin, LOW);
+    delayMicroseconds(5);
     digitalWrite(SCLpin, HIGH);
-    digitalWrite(SDApin, HIGH);
     delayMicroseconds(5);
     digitalWrite(SCLpin, LOW);
 
-    return c;
+    return dec+48;
 }
 
 void charConversion(const char* msg) {
@@ -84,6 +102,29 @@ void charConversion(const char* msg) {
     }
 }
 
+void SlaveAdressWrite (String ADDRESS1) { 
+    pinMode(SCLpin, OUTPUT);
+    digitalWrite(SCLpin, LOW);
+    pinMode(SDApin, OUTPUT);
+    for(int i=0; i<ADDRESS1.length(); i++) {
+        char v = ADDRESS1[i];
+        digitalWrite(SDApin, ((v=='1')? HIGH : LOW));
+        delayMicroseconds(5);
+        digitalWrite(SCLpin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(SCLpin, LOW);
+        delayMicroseconds(5);
+        digitalWrite(SDApin, LOW);
+        delayMicroseconds(5);
+    }
+    pinMode(SDApin, INPUT);
+    digitalRead(SDApin);
+    delayMicroseconds(5);
+    digitalWrite(SCLpin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(SCLpin, LOW);
+}
+
 
 void loop() {
     delay(10);
@@ -91,14 +132,14 @@ void loop() {
     if(Serial.available()) {
         String sendData = Serial.readStringUntil('\n');
         i2cStart();
-        i2cWriteData((ADDRESS1 << 1) | 0); 
+        SlaveAdressWrite("10000000");
         charConversion(sendData.c_str());
         i2cEnd();
         delay(10);
 
     } else {
         i2cStart();
-        i2cWriteData( (ADDRESS1 <<1 ) | 1); 
+        SlaveAdressWrite("10000001");
         char receivedData = i2cReadData();
         Serial.print(receivedData);
         i2cEnd();
